fe722a8eb1165f76a9fb183fff24c85993cdf4df
Jeremy-Burns0212 <208514124+Jeremy-Burns0212@users.noreply.github.com>
2026-02-18 13:53:48 -0800

    Generate Template Test Project

---
+++ BankAccount.UnitTests/ValidatorTests.cs
@@ -0,0 +1,27 @@
+﻿#nullable enable
+using BankAccount;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections;
+
+
+namespace BankAccount.UnitTests
+{
+    /// <summary>
+    /// Tests for BankAccount.Validator
+    /// </summary>
+    [TestClass]
+    public class ValidatorTests
+    {
+        /// <summary>
+        /// Verifies that values strictly less than the minimum boundary are treated as outside the range.
+        /// Condition: value &lt; min for several numeric examples (including large negative literal).
+        /// Expected: Method returns true indicating the value is outside the inclusive range.
+        /// </summary>
+        [TestMethod()]
+        public void IsWithinRangeTest()
+        { 
+            Assert.Fail();
+        }
+    }
+}
